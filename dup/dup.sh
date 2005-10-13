19006512299e23ffc99e195e02f6e3e0d544ad3b
nomed <nomed@5582ec5f-4b01-0410-b056-b4007bfd93e7>
2005-10-13 03:49:17 +0000

    * added dup.sh script to upgrade DSS-Live testing

---
+++ dup/dup.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# DSS-LV 
+#
+# dup.sh: script to upgrade DSS-Live testing
+#
+# Daniele Favara <danjele@gmail.com> 2005
+# <http://dss.berlios.de>
+
+# check for new version of of dup.sh
+echo "Checking dup.sh version"
+wget -nv http://svn.berlios.de/viewcvs/*checkout*/dss/dup/dup.md5sum
+
+[ -f dup.md5sum ] && ver=$(cat dup.md5sum | awk '{print $1}' )
+[-f dup.sh ] && md5dup=$(md5sum dup.sh | awk '{print $1}')
+if [ "$ver" != "$md5sum" ];then
+	echo "I found a new version of dup.sh download it from:"
+	echo "http://svn.berlios.de/viewcvs/*checkout*/dss/dup/dup.sh"
+	echo "and run again the script"
+	exit 0
+else
+[-f dup.sh ] && [ -f dup.md5sum ] && ]echo "[Ok]"
+fi
+
+MIRROR=ftp://ftp.berlios.de/pub/dss/preA/
+MD5SUM=cdrom/md5sums
+md5sum `find -type f | cut -d/ -f2- `> md5sums.old
+echo "Downloading md5sums from $MIRROR"
+wget -x -nv --progress=dot -nH --cut-dirs=3 $MIRROR/$MD5SUM
+:> wget.list
+while read line
+do 
+md5=$(echo $line | awk '{print $1}')
+file=$(echo $line | awk '{print $2}')
+if [ "$file" != "$MD5SUM" ];then
+	if [ -e "$file" ];then
+		md5test=$(md5sum $file | awk '{print $1}')
+		if [ $md5 != "$md5test"  ];then
+		upgrades="$upgrades $file"
+		echo $MIRROR/$file >> wget.list
+	fi
+	else
+		newfiles="$newfiles $file"
+		echo $MIRROR/$file >> wget.list
+	fi
+fi
+done < $MD5SUM
+
+empty="$upgrades$newfiles"
+if [ "$empty" != "" ];then
+	if [ -n "$upgrades" ];then
+		echo 
+		echo ----------------------------------
+		echo "these files will be upgraded:"
+		listup=$(echo "$upgrades" | tr ' ' '\n')
+		echo "$listup"
+		echo
+		echo ----------------------------------
+	fi
+	if [ -n "$newfiles" ];then
+		echo "these files will be downloaded:"
+		listnew=$(echo "$newfiles" | tr ' ' '\n')
+		echo "$listnew"
+		echo
+		echo ----------------------------------
+	fi
+	echo -n "Should I upgrade?: [N/y]"
+	read ansfer
+	if [ "$ansfer" = "n" ];then
+        	exit 0
+	elif [ "$ansfer" = "N" ];then
+	        exit 0
+	elif [ "$ansfer" = "y" ];then
+	        echo
+	        echo "Starting"
+	        echo
+	elif [ "$ansfer" = "Y" ];then
+	        echo
+	        echo "Starting"
+	        echo
+	else
+        	exit 0
+	fi
+	wget  -c -x   -nH --cut-dirs=3  -i wget.list
+else
+	echo
+	echo ----------------------------------	
+	echo "nothing to upgrade"
+	echo ----------------------------------	
+	echo
+fi
